ed15d5c3bf16b1003c6f155b1f1ea28803500acd
Nuno Aguiar <nunoaguiar2003@gmail.com>
2025-03-14 20:20:55 +0000

    Incompleto

---
+++ TPC4/routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Engenharia Web 2025',
+    docente: 'JCR',
+    instituicao: 'Uminho'
+  });
+});
+
+/* GET Filmes List */
+router.get('/filmes', function(req, res) {
+  axios.get('http://localhost:5000/filmes')
+    .then(resp => {
+      res.render('filmes', {
+        lfilmes: resp.data,
+        tit: "Lista de Filmes"
+      });
+    })
+    .catch(erro => {
+      console.log(erro);
+      res.render('error', { error: erro });
+    });
+});
+
+/* GET Filme Edit Page */
+router.get('/filmes/edit/:id', function(req, res) {
+  axios.get(`http://localhost:5000/filmes/${req.params.id}`)
+    .then(resp => {
+      res.render('filme_edit', { filme: resp.data });
+    })
+    .catch(erro => {
+      console.log(erro);
+      res.render('error', { error: erro });
+    });
+});
+
+/* POST Filme Update */
+router.post('/filmes/update/:id', async (req, res) => {
+  try {
+    const filmeId = req.params.id;
+    const { title, year, cast, genres } = req.body;
+
+    const castArray = cast.split(',').map(s => s.trim());
+    const genresArray = genres.split(',').map(s => s.trim());
+
+    const updatedFilme = {
+      title,
+      year: parseInt(year),
+      cast: castArray,
+      genres: genresArray
+    };
+
+    await axios.put(`http://localhost:5000/filmes/${filmeId}`, updatedFilme);
+    res.redirect('/filmes');
+  } catch (error) {
+    console.error('Erro ao atualizar filme:', error);
+    res.status(500).send('Erro ao atualizar o filme');
+  }
+});
+
+/* GET Filme by Actor */
+router.get('/filmes/actor/:nome', function(req, res) {
+  axios.get('http://localhost:5000/filmes')
+    .then(resp => {
+      const filmes_autor = resp.data.filter(f => f.cast.includes(req.params.nome));
+      res.render('ator', { filmes: filmes_autor, autor: req.params.nome });
+    })
+    .catch(erro => {
+      console.log(erro);
+      res.render('error', { error: erro });
+    });
+});
+
+/* DELETE Filme */
+router.get('/filmes/delete/:id', function(req, res) {
+  axios.delete(`http://localhost:5000/filmes/${req.params.id}`)
+    .then(() => {
+      res.redirect('/filmes');
+    })
+    .catch(erro => {
+      console.log(erro);
+      res.render('error', { error: erro });
+    });
+});
+
+module.exports = router;
